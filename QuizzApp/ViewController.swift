818788bfc2811cd1740afbed7fd1f2cb3e38e851
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-05 21:33:38 +0300

    Initial Commit

---
+++ QuizzApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzApp
+//
+//  Created by Murat Ceyhun Korpeoglu on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
